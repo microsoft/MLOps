3d0e8f59ae20f031917b6b9ee2429ef95c35a150
Jordan Edwards <jordane@outlook.com>
2019-05-04 13:47:41 -0700

    Create deploy-model.py

---
+++ examples/explainability/deploy-model.py
@@ -0,0 +1,18 @@
+from azureml.core.model import Workspace, InferenceConfig, Model
+from azureml.core.webservice import AciWebservice
+
+scoring_explainer_model = Model(ws, 'IBM_attrition_explainer')
+attrition_model = Model(ws, 'attrition_model')
+
+aciconfig = AciWebservice.deploy_configuration(cpu_cores=1, 
+                                               memory_gb=1, 
+                                               tags={"data": "IBM_Attrition",  
+                                                     "method" : "local_explanation"}, 
+                                               description='Explain predictions on employee attrition')
+                                               
+inference_config = InferenceConfig(entry_script='score.py', 
+                                   extra_docker_file_steps='dockerfile', 
+                                   runtime='python', 
+                                   conda_file='myenv.yml')
+
+service = Model.deploy(ws, name='predictattrition', models=[scoring_explainer_model, attrition_model], inference_config= inference_config, deployment_config=aciconfig)
